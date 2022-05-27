1df40e24b1dc5076994bb232337ba076f88131c0
Hardik Jain <92137617+Jainakin@users.noreply.github.com>
2022-05-27 23:50:03 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:back_to_monke/screens/home_screen.dart';
+
+void main() => runApp(Hackaccino());
+
+
+class Hackaccino extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
